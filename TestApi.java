d71ced773938b4af2fcbb68b5963a5b125a210e2
NidhiNagdeve <100594491+NidhiNagdeve@users.noreply.github.com>
2022-03-04 17:40:52 +0530

    Add files via upload

---
+++ TestApi.java
@@ -0,0 +1,14 @@
+package junitexamples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestApi {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
